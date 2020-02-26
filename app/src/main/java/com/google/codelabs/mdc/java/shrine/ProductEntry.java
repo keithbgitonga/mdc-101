83e7528f0925c3cb67afe5b3766cfb501abb9bcf
keith <keithbrightgitonga@gmail.com>
2020-02-26 22:26:37 +0300

    Assignment complete

---
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductEntry.java
@@ -0,0 +1,10 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.content.res.Resources;
+
+import java.util.List;
+
+class ProductEntry {
+    public static List<com.google.codelabs.mdc.java.shrine.network.ProductEntry> initProductEntryList(Resources resources) {
+    }
+}
